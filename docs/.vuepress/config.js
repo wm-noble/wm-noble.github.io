d3ab7ba57402d5dcfb22a60c01d8914ca464a516
Will <author@will.party>
2022-05-16 21:51:01 -0700

    Strip the index

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,5 +3,8 @@ const { defaultTheme } = require('@vuepress/theme-default')
 module.exports = {
   theme: defaultTheme({
     sidebar: false,
+    navbar: false,
+    contributors: false,
+    lastUpdated: false,
   }),
 }
